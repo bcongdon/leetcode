a7153b72093682460841dae4614e3039120e65bd
Benjamin Congdon <bencongdon96@gmail.com>
2016-09-28 06:39:51 -0500

    237, 383

---
+++ 200-299/237-delete-node-in-linked-list.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        if node.next:
+            node.val = node.next.val
+            node.next = node.next.next
---
+++ 300-399/383-ransom-note.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+
+class Solution(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        return not Counter(ransomNote) - Counter(magazine)
