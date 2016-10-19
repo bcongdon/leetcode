a6bf406c044ed7ed9e4a6910407dff01a9c6bd17
Benjamin Congdon <bencongdon96@gmail.com>
2016-10-19 06:28:02 -0500

    349

---
+++ 300-399/349-intersection-of-two-arrays.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        res = []
+        for i in nums1:
+            if i in nums2 and i not in res:
+                res.append(i)
+        return res
